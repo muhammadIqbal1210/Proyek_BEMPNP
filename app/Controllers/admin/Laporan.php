5bcce8b203ed40b880fa4579d9d29a38e0730bcf
muhammadIqbal1210 <iqbalmuhammad07015@gmail.com>
2026-01-27 14:42:25 +0700

    Perbaiki logika Hapus

--- app/Controllers/admin/Laporan.php
+++ app/Controllers/admin/Laporan.php
@@ -7,7 +7,7 @@ use App\Models\LaporanModel;
 
 class Laporan extends BaseController
     {
-    protected $LaporanModel;
+    protected $laporanModel;
 
     public function __construct()
     {
@@ -22,33 +22,6 @@ class Laporan extends BaseController
         $filters = ['keyword' => $keyword];
         
         $query = $this->laporanModel;
-
-        // // Simulasi Data (Ganti dengan $this->advokasiModel sesuai logika beasiswa Anda)
-        // $laporan = [
-        //     [
-        //         'title' => 'Laporan Advokasi',
-        //         'id' => 1,
-        //         'nama' => 'Budi Santoso',
-        //         'nim' => '21010122',
-        //         'kategori' => 'Fasilitas',
-        //         'isi' => 'AC di ruang kelas 3.2 mati total sejak seminggu lalu.',
-        //         'kontak' => '08123456789',
-        //         'status' => 'Masuk',
-        //         'lampiran' => 'lampiran_1.jpg',
-        //         'created_at' => '2023-10-24 08:30:00'
-        //     ],
-        //     [
-        //         'id' => 2,
-        //         'nama' => 'Siti Aminah',
-        //         'nim' => '21010145',
-        //         'kategori' => 'Akademik',
-        //         'isi' => 'Nilai mata kuliah Basis Data belum keluar di KHS.',
-        //         'kontak' => '08571234567',
-        //         'status' => 'Proses',
-        //         'lampiran' => '',
-        //         'created_at' => '2023-10-23 14:20:00'
-        //     ]
-        // ];
         $perPage = 10;
         $laporan_list = $query->paginate($perPage, 'laporan');
         $pager = $query->pager;
@@ -81,7 +54,7 @@ class Laporan extends BaseController
     public function delete($id)
     {
         // Logika hapus
-        // $this->advokasiModel->delete($id);
+        $this->laporanModel->delete($id);
 
         return redirect()->back()->with('success', 'Data pengaduan berhasil dihapus.');
     }
